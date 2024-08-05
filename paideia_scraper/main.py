829b15bdbcfae5ef8069df18823af7f59648f78b
Eric Schrock <eschrock@gmail.com>
2024-08-05 19:40:15 -0400

    get students

--- paideia_scraper/main.py
+++ paideia_scraper/main.py
@@ -2,8 +2,13 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
 import os
 import sys
+import json
+import time
+from selenium.common.exceptions import NoSuchElementException
+
 
 STUDENT_DIRECTORY_URL = "https://www.paideiaschool.org/parent-portal/student-directory"
 
@@ -30,20 +35,58 @@ def login():
     return driver
 
 
+def get_current_group_id(driver):
+    pagination_elem = driver.find_element(By.CLASS_NAME, "fsElementPagination")
+    raw_params = pagination_elem.get_attribute("data-searchparams")
+    search_params = json.loads(raw_params)
+    return search_params["const_search_location"]
+
+
 def get_class_students(driver, class_name):
-    # Wait until "Location" is visible
+    # Make sure "Location" is visible, as after login
     WebDriverWait(driver, 600).until(
         EC.presence_of_element_located((By.NAME, "const_search_location"))
     )
 
     # Set the location
+    select_elem = driver.find_element(By.NAME, "const_search_location")
+    select = Select(select_elem)
+    select.select_by_visible_text(class_name)
+    group_id = select.first_selected_option.get_attribute("value")
+    print(f"Looking up students in class '{class_name}' ({group_id})")
+    select_elem.submit()
+
+    # Make sure the given clas is loaded
+    current_group_id = get_current_group_id(driver)
+    while current_group_id != group_id:
+        time.sleep(1)
+        current_group_id = get_current_group_id(driver)
+
+    # Fetch all student elements
+    students = {}
+    student_elem_list = driver.find_elements(By.CLASS_NAME, "fsConstituentProfileLink")
+    for student_elem in student_elem_list:
+        try:
+            # This class appears twice, ignore the second case with a child span
+            student_elem.find_element(By.TAG_NAME, "span")
+        except NoSuchElementException:
+            student_name = student_elem.text.strip()
+            print(f"Found student {student_name}")
+            students[student_name] = student_elem
+
+    return students
+
+
+def get_parent_info(driver, students):
+    pass
 
 
 def main() -> int:
     if len(sys.argv) != 2:
         exit("usage: scrape <class name>")
     driver = login()
-    get_class_students(driver, sys.argv[1])
+    students = get_class_students(driver, sys.argv[1])
+    get_parent_info(driver, students)
     return 0
 
 
